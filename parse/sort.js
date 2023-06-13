1314b72a286282415ef49a99ca7d404594904cae
banday <oncwnuMRO3XXqgofNgUPylgUnhUk@git.weixin.qq.com>
2023-06-13 21:58:30 +0800

    add func

---
+++ parse/sort.js
@@ -0,0 +1,29 @@
+// 排序
+const arr = [1, 3, 2];
+arr.sort(); // [1, 2, 3]
+
+// 转换数组
+const arr = [1, 2, 3];
+const newArr = arr.map((item) => item * 2); // [2, 4, 6]
+
+// 对象
+const arr = [
+    { name: "John", age: 25 },
+    { name: "Jane", age: 30 },
+    ];
+arr.sort((a, b) => a.age - b.age); // [{ name: "John", age: 25 }, { name: "Jane", age: 30 }]
+
+// 排序
+const arr = [1, 3, 2];
+arr.sort(); // [1, 2, 3]
+
+// 转换数组
+const arr = [1, 2, 3];
+const newArr = arr.map((item) => item * 2); // [2, 4, 6]
+
+// 对象
+const arr = [
+    { name: "John", age: 25 },
+    { name: "Jane", age: 30 },
+    ];
+arr.sort((a, b) => a.age - b.age); // [{ name: "John", age: 25 }, { name: "Jane", age: 30 }]
